4b8ca60ac33699aaf23999ccaaca9dbf3831b490
Doja-oualla <dojaoualla@gmail.com>
2024-12-18 14:56:37 +0100

    conection mysql

---
+++ config/database.php
@@ -0,0 +1,22 @@
+<?php
+
+$servarname = "localhost";
+$username = "root";
+$password = "";
+$dbname= "FootballDB";
+
+
+//create connection
+
+$conn = mysqli_connect($servarname, $username, $password,$dbname);
+
+if(!$conn){
+    die("Connection failed :" .mysqli_connect_error());
+}
+
+echo"Connected successfully";
+
+
+?>
+
+
